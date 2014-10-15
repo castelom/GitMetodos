7d281f1ee85e37f02f9c8d013ba1ce9032db8389
Kat2 <katysombra@gmail.com>
2014-10-15 10:13:18 -0300

    Commit tentativa de conflito

--- src/ifce/Contador.java
+++ src/ifce/Contador.java
@@ -1,9 +1,19 @@
 package ifce;
 
 public class Contador {
+
 	public void ContaPares(int num){
 		int i;
 		for(i = 0; i<= num; i = i+2){
+			System.out.println(i);
+		}
+	}
+	public void ContaPares_e_Impares(int num){
+		int i;
+		for(i = 0; i<= num; i = i+2){
+			System.out.println(i);
+		}
+		for(i = 1; i<= num; i = i+2){
 			System.out.println(i);
 		}
 	}
